a5684a2a19922a5b47f56a2158c83186e73fdfcc
ArcangeliSammy <samuele.arcangeli@studenti.ittsrimini.edu.it>
2022-03-28 09:02:56 +0200

    Completamento form articolo con: property di sola lettura, attributi privati e controllo dei dati inseriti

--- AS2122_4H_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4H_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -16,11 +16,19 @@ namespace AS2122_4H_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
         // ...
+        private string descrizione;
+
+        private string unitaMisura;
+
+        private double prezzo;
 
         public DialogResult Status { get { return status; } }
 
         // TODO: (6) aggiungere property di sola lettura dei dati inseriti nella frmArticoli per l'utilizzo in frmMain
         // ...
+        public string Descrizione {  get { return descrizione; } }
+        public string UnitaMisura {  get { return unitaMisura; } }
+        public double Prezzo {  get { return prezzo; } }
 
         public frmArticolo()
         {
@@ -32,10 +40,22 @@ namespace AS2122_4H_INF_Prof_ProvaGestioneArticoli
             // TODO: (7) passaggio all' attributo/property dei dati inseriti nella frmArticoli con controllo di valorizzazione del dato
             // ... descrizione, unitaMisura, prezzo
 
+            if (txtDescrizione.Text == "" || cmbUnitaMisura.SelectedItem.ToString() == "" || txtPrezzo.Text == "")
+            {
+                MessageBox.Show("I dati inseriti non sono corretti");
+            }
+            else
+            {
+                descrizione = txtDescrizione.Text;
 
+                unitaMisura = cmbUnitaMisura.SelectedItem.ToString();
 
-            status = DialogResult.OK;
-            Close();
+                prezzo = Convert.ToDouble(txtPrezzo.Text);
+
+                status = DialogResult.OK;
+                Close();
+            }
+            
         }
 
         private void btnAnnulla_Click(object sender, EventArgs e)
